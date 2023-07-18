db29a887e517170cee69d35271b8703423e8d1df
PAULGN1992 <47513033@continental.edu.pe>
2023-07-18 10:39:55 -0500

    ...

--- client/src/views/Form/form.jsx
+++ client/src/views/Form/form.jsx
@@ -9,17 +9,61 @@ function validate(pokemonData) {
 
   if (!pokemonData.Nombre) {
     errores.Nombre = "Ingrese nombre de pokemon";
+  } else if (!/^[A-Za-z\s]+$/.test(pokemonData.Nombre)) {
+    errores.Nombre = "Solo se permiten letras";
   }
   
   if (pokemonData.Nombre.length >= 20) {
     errores.Nombre = "Maximo 20 caracteres";
-  }
+  } 
 
-  if (pokemonData.Vida < 10 || pokemonData.Vida > 200) {
+  if (pokemonData.Vida < 10 || pokemonData.Vida > 201) {
     errores.Vida = "Debe ser mayor a 10 y menor de 200";
   }
   if (!pokemonData.Vida) {
     errores.Vida = "Ingrese Vida";
+  }
+  
+  if (pokemonData.Ataque < 10 || pokemonData.Ataque > 201) {
+    errores.Ataque = "Debe ser mayor a 10 y menor de 200";
+  }
+  if (!pokemonData.Ataque) {
+    errores.Ataque = "Ingrese Ataque";
+  }
+
+  if (pokemonData.Defensa < 10 || pokemonData.Defensa > 201) {
+    errores.Defensa = "Debe ser mayor a 10 y menor de 200";
+  }
+  if (!pokemonData.Defensa) {
+    errores.Defensa = "Ingrese Defensa";
+  }
+
+  if (pokemonData.Velocidad < 10 || pokemonData.Velocidad > 201) {
+    errores.Velocidad = "Debe ser mayor a 10 y menor de 200";
+  }
+  if (!pokemonData.Velocidad) {
+    errores.Velocidad = "Ingrese Velocidad";
+  }
+
+  if (pokemonData.Altura < 0.1 || pokemonData.Altura > 15) {
+    errores.Altura = "Debe ser mayor 0.1m y menor a 15 metros";
+  }
+  if (!pokemonData.Altura) {
+    errores.Altura = "Ingrese Altura";
+  }
+
+  if (pokemonData.Altura < 0.1 || pokemonData.Altura > 15) {
+    errores.Altura = "Debe ser mayor 0.1m y menor a 15 metros";
+  }
+  if (!pokemonData.Altura) {
+    errores.Altura = "Ingrese Altura";
+  }
+
+  if (pokemonData.Peso > 500) {
+    errores.Peso = "Debe menor a 500 kg";
+  }
+  if (!pokemonData.Peso) {
+    errores.Peso = "Ingrese Peso";
   }
 
   return errores;
@@ -41,43 +85,62 @@ function Form() {
     Peso: '',
     Tipos: []
     });
-    const [showError, setShowError] = useState(false);
-  const [showCheck, setShowCheck] = useState(false);
-  const [namePokemonExists, setNamePokemonExists] = useState(false);
-  const [errores, setErrores] = useState({
-    Nombre: '',
-    Vida: '',
-  });
+    const [state, setState] = useState({
+      showError: false,
+      showCheck: false,
+      namePokemonExists: false,
+      errores: {},
+    });
+    
+
   useEffect(() => {
     const checkNameExists = () => {
-      const nombreExists = allPokemons.some(pokemon => pokemon.Nombre === pokemonData.Nombre.toUpperCase());
-      setNamePokemonExists(nombreExists);
+    const nombreExists = allPokemons.some(
+      (pokemon) => pokemon.Nombre === pokemonData.Nombre.toUpperCase()
+    );
+      setState((prevState) => ({
+        ...prevState,
+        namePokemonExists: nombreExists,
+      }));
     };
 
-    // Verificar si el nombre ya existe al cambiar el valor del nombre del Pokemon
-    checkNameExists();
+  checkNameExists();
   }, [pokemonData.Nombre, allPokemons]);
 
   useEffect(() => {
     if (errors === 'No se pudo crear al pokemon') {
-      setShowError(true);
+      setState((prevState) => ({
+        ...prevState,
+        showError: true,
+      }));
+  
       const timer = setTimeout(() => {
-        setShowError(false);
-        dispatch(resetPokemons())
-      }, 2000); // Mostrar el mensaje de error durante 3 segundos
-
-      return () => clearTimeout(timer); // Limpiar el temporizador cuando el componente se desmonte
+        setState((prevState) => ({
+          ...prevState,
+          showError: false,
+        }));
+        dispatch(resetPokemons());
+      }, 2000);
+  
+      return () => clearTimeout(timer);
     }
   }, [errors]);
 
   useEffect(() => {
     if (check) {
-      setShowCheck(true);
-      const timer = setTimeout(() => {
-        setShowCheck(false);
-      }, 2000); // Mostrar el mensaje de éxito durante 3 segundos
+      setState((prevState) => ({
+        ...prevState,
+        showCheck: true,
+      }));
 
-      return () => clearTimeout(timer); // Limpiar el temporizador cuando el componente se desmonte
+      const timer = setTimeout(() => {
+        setState((prevState) => ({
+          ...prevState,
+          showCheck: false,
+        }));
+      }, 2000);
+
+      return () => clearTimeout(timer);
     }
   }, [check]);
 
@@ -89,18 +152,32 @@ function Form() {
       [name]: value
     }));
 
-    setErrores(
-      validate({
+    setState((prevState) => ({
+      ...prevState,
+      errores: validate({
         ...pokemonData,
         [name]: value,
-      })
-    );
+      }),
+    }));
   };
+
   const handleTypeSelection = (event) => {
     const { value } = event.target;
 
-    if (pokemonData.Tipos.length >= 3 || pokemonData.Tipos.includes(value)) {
-        return alert('Error')// No agregar más tipos o evitar repeticiones
+    if (pokemonData.Tipos.length >= 3) {
+        setState((prevData)=>({
+          ...prevData,
+          errores: {
+            Tipos: "Maximo 3 tipos "
+          }
+        }))
+      } else if (pokemonData.Tipos.includes(value)) {
+        setState((prevData)=>({
+          ...prevData,
+          errores: {
+            Tipos: "Ya existe ese tipo"
+          }
+        })) 
       }
     setPokemonData((prevData) => ({
       ...prevData,
@@ -116,17 +193,9 @@ function Form() {
   };
   const handleSubmit = async (event) => {
     event.preventDefault();
-
-    // Realizar validaciones de los campos ingresados
-    if (!pokemonData.Nombre) {
-      // Mostrar mensajes de error o realizar alguna acción cuando los campos no sean válidos
-      return alert('error. faltan datos');
-    }
-    
+  
     const altura = pokemonData.Altura ? pokemonData.Altura * 10 : '';
     const peso = pokemonData.Peso ? pokemonData.Peso * 10 : '';
-
-    // Aquí puedes hacer algo con los datos ingresados, como enviarlos a un servidor o procesarlos de alguna manera
 
     dispatch(createPokemon({
       Nombre: pokemonData.Nombre.toUpperCase(),
@@ -160,33 +229,38 @@ function Form() {
       <div>
         <label>Nombre:</label>
         <input type="text" id="Nombre" name="Nombre" value={pokemonData.Nombre} onChange={handleInputChange} />
-        {errores.Nombre ? <p>{errores.Nombre}</p> : null}
-        {namePokemonExists && <p>Ya existe el nombre del pokemon</p>}
+        {state.errores.Nombre && <p>{state.errores.Nombre}</p>}
+        {state.namePokemonExists && <p>Ya existe el nombre del pokemon</p>}
       </div>
       <div>
         <label>Vida:</label>
         <input type="number" id="Vida" name="Vida" value={pokemonData.Vida} onChange={handleInputChange} />
-        {errores.Vida ? <p>{errores.Vida}</p>: null}     
+        {state.errores.Vida && <p>{state.errores.Vida}</p>}   
       </div>
       <div>
         <label>Ataque:</label>
         <input type="number" id="Ataque" name="Ataque" value={pokemonData.Ataque} onChange={handleInputChange} />
+        {state.errores.Ataque && <p>{state.errores.Ataque}</p>}
       </div>
       <div>
         <label>Defensa:</label>
         <input type="number" id="Defensa" name="Defensa" value={pokemonData.Defensa} onChange={handleInputChange} />
+        {state.errores.Defensa && <p>{state.errores.Defensa}</p>}
       </div>
       <div>
         <label>Velocidad:</label>
         <input type="number" id="Velocidad" name="Velocidad" value={pokemonData.Velocidad} onChange={handleInputChange} />
+        {state.errores.Velocidad && <p>{state.errores.Velocidad}</p>}
       </div>
       <div>
         <label>Altura(metros):</label>
         <input type="number" id="Altura" name="Altura" value={pokemonData.Altura} onChange={handleInputChange} />
+        {state.errores.Altura && <p>{state.errores.Altura}</p>}
       </div>
       <div>
         <label>Peso(kg):</label>
         <input type="number" id="Peso" name="Peso" value={pokemonData.Peso} onChange={handleInputChange} />
+        {state.errores.Peso && <p>{state.errores.Peso}</p>}
       </div>
       <div>
         <span>
@@ -204,6 +278,7 @@ function Form() {
             ))}
           </select>
         </span>
+        {state.errores.Tipos && <p>{state.errores.Tipos}</p>}
         <ul>
           {pokemonData.Tipos.map((type, index) => (
             <li key={index}>
@@ -213,9 +288,9 @@ function Form() {
           ))}
         </ul>
       </div>
-      <button onClick={handleSubmit}>CREAR</button>
-      {showError && <p>{errors} </p>}
-      {showCheck && <p>{check}</p>}
+      {Object.keys(state.errores).length === 0 && (<button onClick={handleSubmit}>CREAR</button>)}
+      {state.showError && <p>{errors} </p>}
+      {state.showCheck && <p>{check}</p>}
     </div>
   );
 }
